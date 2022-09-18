086a00f8af0bb289bbb263ce9a4e865ce5a28692
Micoder-dev <micoder.com@gmail.com>
2022-09-19 01:39:43 +0530

    FloatingNavigationView Implemented

--- app/src/main/java/com/micoder/dpslive/ui/MainActivity.kt
+++ app/src/main/java/com/micoder/dpslive/ui/MainActivity.kt
@@ -2,14 +2,18 @@ package com.micoder.dpslive.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.andremion.floatingnavigationview.FloatingNavigationView
 import com.micoder.dpslive.R
 import com.micoder.dpslive.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var mFloatingNavigationView: FloatingNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +23,25 @@ class MainActivity : AppCompatActivity() {
         val fragment = supportFragmentManager.findFragmentById(R.id.dpsNavHostFragment)
         binding.bottomNavigationView.setupWithNavController(fragment!!.findNavController())
 
+        fab()
+
+    }
+
+    // Floating Navigation View
+    private fun fab() {
+        mFloatingNavigationView = binding.floatingNavigationView
+        mFloatingNavigationView.setOnClickListener { mFloatingNavigationView.open() }
+        mFloatingNavigationView.setNavigationItemSelectedListener { item ->
+            Toast.makeText(this, item.title.toString() + " Selected!", Toast.LENGTH_SHORT).show()
+            mFloatingNavigationView.close()
+            true
+        }
+    }
+    override fun onBackPressed() {
+        if (mFloatingNavigationView.isOpened) {
+            mFloatingNavigationView.close()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
