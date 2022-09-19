6f123ec0c1054d1639c6ed981968192d3df30a83
Micoder-dev <micoder.com@gmail.com>
2022-09-20 00:56:37 +0530

    resultFrag added with anim, navGraph updated

--- app/src/main/java/com/micoder/dpslive/ui/fragments/DashboardFragment.kt
+++ app/src/main/java/com/micoder/dpslive/ui/fragments/DashboardFragment.kt
@@ -3,12 +3,21 @@ package com.micoder.dpslive.ui.fragments
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.micoder.dpslive.R
+import com.micoder.dpslive.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
 
+    private var binding: FragmentDashboardBinding? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentDashboardBinding.bind(view)
+
+        binding!!.result.setOnClickListener {
+            findNavController().navigate(R.id.action_dashboardFragment_to_cardResultFragment)
+        }
     }
 
 }
