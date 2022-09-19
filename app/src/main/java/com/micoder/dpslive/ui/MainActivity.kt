e359ea195b0cbe625291ce841c718faee2c47b50
Micoder-dev <micoder.com@gmail.com>
2022-09-19 20:50:08 +0530

    custom Day/Night nav item added

--- app/src/main/java/com/micoder/dpslive/ui/MainActivity.kt
+++ app/src/main/java/com/micoder/dpslive/ui/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.micoder.dpslive.ui
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CompoundButton
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.andremion.floatingnavigationview.FloatingNavigationView
+import com.google.android.material.switchmaterial.SwitchMaterial
 import com.micoder.dpslive.R
 import com.micoder.dpslive.databinding.ActivityMainBinding
 
@@ -15,16 +21,28 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var mFloatingNavigationView: FloatingNavigationView
 
+    private lateinit var switchMaterial: SwitchMaterial
+    private val MyPREFERENCES: String = "nightModePrefs"
+    private val KEY_ISNIGHTMODE: String = "isNightMode"
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val fragment = supportFragmentManager.findFragmentById(R.id.dpsNavHostFragment)
-        binding.bottomNavigationView.setupWithNavController(fragment!!.findNavController())
+        bottomNavigation()
 
         fab()
 
+        dayNight()
+
+    }
+
+    // Bottom Navigation View
+    private fun bottomNavigation() {
+        val fragment = supportFragmentManager.findFragmentById(R.id.dpsNavHostFragment)
+        binding.bottomNavigationView.setupWithNavController(fragment!!.findNavController())
     }
 
     // Floating Navigation View
@@ -42,6 +60,47 @@ class MainActivity : AppCompatActivity() {
             mFloatingNavigationView.close()
         } else {
             super.onBackPressed()
+        }
+    }
+
+    // Day Night themes
+    private fun dayNight() {
+        // Shared pref
+        sharedPreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE)
+
+        // navigationView night mode switch control
+        val menuItem = mFloatingNavigationView.menu.findItem(R.id.nightModeFab) // first initialize MenuItem
+
+        switchMaterial = menuItem.actionView.findViewById<View>(R.id.drawer_switch) as SwitchMaterial
+
+        checkNightModeActivated()
+
+        switchMaterial.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
+            if (isChecked) {
+                Toast.makeText(this, "Dark Mode Enabled", Toast.LENGTH_SHORT).show()
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                saveNightModeState(true)
+                recreate()
+            } else {
+                Toast.makeText(this, "Dark Mode Disabled", Toast.LENGTH_SHORT).show()
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                saveNightModeState(false)
+                recreate()
+            }
+        }
+    }
+    private fun saveNightModeState(nightMode: Boolean) {
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+        editor.putBoolean(KEY_ISNIGHTMODE, nightMode)
+        editor.apply()
+    }
+    private fun checkNightModeActivated() {
+        if (sharedPreferences.getBoolean(KEY_ISNIGHTMODE, false)) {
+            switchMaterial.isChecked = true
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }else{
+            switchMaterial.isChecked = false
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         }
     }
 }
